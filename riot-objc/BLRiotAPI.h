ae0340ca49223a27e9bce5355093a337fd8a07c7
Ash K <im420blaziken@gmail.com>
2013-12-17 12:24:30 -0600

    Fixed endpoint string format specifiers.

--- riot-objc/BLRiotAPI.h
+++ riot-objc/BLRiotAPI.h
@@ -16,19 +16,19 @@
 // game-v1.1 [EUW, EUNE, NA]
 #define API_GAME                API_ENDPOINT @"/api/lol/%@/v1.1/game/by-summoner/%ld/recent"
 // league-v2.1 [EUNE, TR, NA, EUW, BR]
-#define API_LEAGUE              API_ENDPOINT @"/api/%@/v2.1/league/by-summoner/%d"
+#define API_LEAGUE              API_ENDPOINT @"/api/%@/v2.1/league/by-summoner/%ld"
 // stats-v1.1 [EUW, NA, EUNE]
-#define API_STATS               API_ENDPOINT @"/api/lol/%@/v1.1/stats/by-summoner/%d"
+#define API_STATS               API_ENDPOINT @"/api/lol/%@/v1.1/stats/by-summoner/%ld"
 #define API_STATS_SUMMARY       API_STATS @"/summary"
 #define API_STATS_RANKED        API_STATS @"/ranked"
 // summoner-v1.1 [EUNE, NA, EUW]
-#define API_SUMMONER            API_ENDPOINT @"/api/lol/%@/v1.1/summoner/%d"
+#define API_SUMMONER            API_ENDPOINT @"/api/lol/%@/v1.1/summoner/%ld"
 #define API_SUMMONER_BYNAME     API_ENDPOINT @"/api/lol/%@/v1.1/summoner/by-name/%@"
 #define API_SUMMONERS           API_ENDPOINT @"/api/lol/%@/v1.1/summoner/%s/name"
 #define API_SUMMONER_MASTERY    API_SUMMONER @"/mastery"
 #define API_SUMMONER_RUNES      API_SUMMONER @"/runes"
 // team-v2.1 [EUNE, EUW, BR, TR, NA]
-#define API_TEAM                API_ENDPOINT @"/api/%@/v2.1/team/by-summoner/%d"
+#define API_TEAM                API_ENDPOINT @"/api/%@/v2.1/team/by-summoner/%ld"
 
 // Errors
 #define AUTHENTICATION_ERROR    @"AuthenticationError"
